01223e676832185b7807af3b3999fad92c547b13
G-Cuevas <cuevasgilberto118@gmail.com>
2023-09-02 19:02:32 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return <div>Hello Worldaaaaaa!</div>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
